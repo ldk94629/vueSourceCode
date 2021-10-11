75f476edcc23d60204f9f41b9abcd3ebed5d2395
ldk94629 <a1251104161@163.com>
2021-10-11 11:12:24 +0800

    612

--- analysisVueSourceCode.js
+++ analysisVueSourceCode.js
@@ -445,3 +445,331 @@ window.Vuw=factory();
     }
   }
 
+  //cached polyfillBind looseEqual 闭包 类型判断 函数之间的互相调用 
+  /************330-----612 */
+  
+  /**
+   * Return the first index at which a loosely equal value can be
+   * found in the array
+   * 返回index
+   *  (if value is a plain object, the array must
+   * contain an object of the same shape),
+   * 如果是对象;则需要在arr中有形状相同的对象
+   *  or -1 if it is not present.
+   * 返回索引;否则执行looseEaual
+   */
+
+   function looseIndexOf (arr, val) {
+    for (var i = 0; i < arr.length; i++) {
+      if (looseEqual(arr[i], val)) { return i }
+    }
+    return -1
+  }
+
+  /**
+   * Ensure a function is called only once.
+   * 保证一个方法只执行一次
+   */
+  function once (fn) {
+    var called = false;
+    return function () {
+      if (!called) {
+        called = true;
+        fn.apply(this, arguments);
+      }
+    }
+  }
+
+  var SSR_ATTR = 'data-server-rendered';//服务端渲染
+
+  var ASSET_TYPES = [
+    'component',//组件
+    'directive',//指令
+    'filter'//过滤函数
+  ];
+//声明周期
+  var LIFECYCLE_HOOKS = [
+    'beforeCreate',
+    'created',
+    'beforeMount',
+    'mounted',
+    'beforeUpdate',
+    'updated',
+    'beforeDestroy',
+    'destroyed',
+    'activated',
+    'deactivated',
+    'errorCaptured',
+    'serverPrefetch'
+  ];
+
+  /*  */
+
+
+//全局配置;
+  var config = ({
+    /**
+     * Option merge strategies (used in core/util/options)
+     */
+    // $flow-disable-line   自定义合并策略选项
+    optionMergeStrategies: Object.create(null),
+
+    /**
+     * Whether to suppress warnings.
+     * 是否制止警告 默认为false;设置为true 将不会有各种报错
+     */
+    silent: false,
+
+    /**
+     * Show production mode tip message on boot?
+     * 开发模式下是否显示生产提示信息
+     */
+    productionTip: "development" !== 'production',
+
+    /**
+     * Whether to enable devtools
+     * 是否允许vue-devtools 检查代码;浏览器环境默认为true;
+     */
+    devtools: "development" !== 'production',
+
+    /**
+     * Whether to record perf
+     * 是否开启性能追踪;只能在开发模式下才支持;
+     */
+    performance: false,
+
+    /**
+     * Error handler for watcher errors
+     * 指定组件的渲染和观察期间未捕获错误的处理函数,可获取错误信息和Vue实例
+     */
+    errorHandler: null,
+
+    /**
+     * Warn handler for watcher warns
+     * 警告处理函数;可以获取错误信息和vue实例
+     */
+    warnHandler: null,
+
+    /**
+     * Ignore certain custom elements
+     * 忽略某些自定义元素;
+     */
+    ignoredElements: [],
+
+    /**
+     * Custom user key aliases for v-on
+     * 给v-on自定义的键位别名;
+     */
+    // $flow-disable-line
+    keyCodes: Object.create(null),
+
+    /**
+     * Check if a tag is reserved so that it cannot be registered as a
+     * component. This is platform-dependent and may be overwritten.
+     * 检测标签是否被保留;如果是不能被注册为组件;
+     */
+    isReservedTag: no,
+
+    /**
+     * Check if an attribute is reserved so that it cannot be used as a component
+     * 检查一个属性是否被保留，以便它不能被用作组件  
+     * prop. This is platform-dependent and may be overwritten.
+     */
+    isReservedAttr: no,
+
+    /**
+     * Check if a tag is an unknown element.
+     * 未知元素
+     * Platform-dependent.
+     */
+    isUnknownElement: no,
+
+    /**
+     * Get the namespace of an element
+     * 命名空间
+     */
+    getTagNamespace: noop,
+
+    /**
+     * Parse the real tag name for the specific platform.
+     */
+    parsePlatformTagName: identity,
+
+    /**
+     * Check if an attribute must be bound using property, 
+     * 检查是否必须绑定属性
+     * e.g. value
+     * Platform-dependent.
+     */
+    mustUseProp: no,
+
+    /**
+     * Perform updates asynchronously. Intended to be used by Vue Test Utils
+     * This will significantly reduce performance if set to false.
+     */
+    async: true,
+
+    /**
+     * Exposed for legacy reasons
+     */
+    _lifecycleHooks: LIFECYCLE_HOOKS
+  });
+
+  /*  */
+
+  /**
+   * unicode letters used for parsing html tags, component names and property paths.
+   * using https://www.w3.org/TR/html53/semantics-scripting.html#potentialcustomelementname
+   * skipping \u10000-\uEFFFF due to it freezing up PhantomJS
+   * 用于解析html标记、组件名称和属性pat的unicode字母
+   */
+  var unicodeRegExp = /a-zA-Z\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C-\u200D\u203F-\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD/;
+
+  /**
+   * Check if a string starts with $ or _
+   * 检查变量的开头是否是$或者_
+   */
+  function isReserved (str) {
+    var c = (str + '').charCodeAt(0);
+    return c === 0x24 || c === 0x5F
+  }
+
+  /**
+   * Define a property.
+   * 在一个对象上定义一个属性的构造函数
+   * @params {Boolean} enumerable
+   */
+  function def (obj, key, val, enumerable) {
+    Object.defineProperty(obj, key, {
+      value: val,
+      enumerable: !!enumerable,
+      writable: true,
+      configurable: true
+    });
+  }
+  
+  /**
+   * Parse simple path.
+   * 解析简单的路径
+   */
+   var bailRE = new RegExp(("[^" + (unicodeRegExp.source) + ".$_\\d]"));
+   function parsePath (path) {
+     if (bailRE.test(path)) {
+       return
+     }
+     var segments = path.split('.');
+     return function (obj) {
+       for (var i = 0; i < segments.length; i++) {
+         if (!obj) { return }
+         obj = obj[segments[i]];
+       }
+       return obj
+     }
+   }
+ 
+   /*  */
+ 
+   // can we use __proto__?
+   是否利用隐式原型
+   var hasProto = '__proto__' in {};
+ 
+   // Browser environment sniffing
+   //判断运行环境;
+   var inBrowser = typeof window !== 'undefined';
+   var inWeex = typeof WXEnvironment !== 'undefined' && !!WXEnvironment.platform;
+   var weexPlatform = inWeex && WXEnvironment.platform.toLowerCase();
+   var UA = inBrowser && window.navigator.userAgent.toLowerCase();
+   var isIE = UA && /msie|trident/.test(UA);
+   var isIE9 = UA && UA.indexOf('msie 9.0') > 0;
+   var isEdge = UA && UA.indexOf('edge/') > 0;
+   var isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 'android');
+   var isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA)) || (weexPlatform === 'ios');
+   var isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge;
+   var isPhantomJS = UA && /phantomjs/.test(UA);
+   var isFF = UA && UA.match(/firefox\/(\d+)/);
+ 
+   // Firefox has a "watch" function on Object.prototype...
+   //火狐的对象属性上拥有一个watch;
+   var nativeWatch = ({}).watch;
+ 
+   var supportsPassive = false;
+   if (inBrowser) {
+     try {
+       var opts = {};
+       Object.defineProperty(opts, 'passive', ({
+         get: function get () {
+           /* istanbul ignore next */
+           supportsPassive = true;
+         }
+       })); // https://github.com/facebook/flow/issues/285
+       window.addEventListener('test-passive', null, opts);
+     } catch (e) {}
+   }
+ 
+   // this needs to be lazy-evaled because vue may be required before
+   //是否为服务端渲染;
+   // vue-server-renderer can set VUE_ENV
+   var _isServer;
+   var isServerRendering = function () {
+     if (_isServer === undefined) {
+       /* istanbul ignore if */
+       if (!inBrowser && !inWeex && typeof global !== 'undefined') {
+         // detect presence of vue-server-renderer and avoid
+         // Webpack shimming the process
+         _isServer = global['process'] && global['process'].env.VUE_ENV === 'server';
+       } else {
+         _isServer = false;
+       }
+     }
+     return _isServer
+   };
+ 
+   // detect devtools
+   var devtools = inBrowser && window.__VUE_DEVTOOLS_GLOBAL_HOOK__;
+ 
+   /**
+    * 
+    *  istanbul ignore next
+    * 判断是否为系统函数;
+    * native;土生土长的;天然的
+    *  */
+   function isNative (Ctor) {
+     return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
+   }
+ 
+   var hasSymbol =
+     typeof Symbol !== 'undefined' && isNative(Symbol) &&
+     typeof Reflect !== 'undefined' && isNative(Reflect.ownKeys);
+ 
+   var _Set;
+   /* istanbul ignore if */ // $flow-disable-line
+   if (typeof Set !== 'undefined' && isNative(Set)) {
+     // use native Set when available.
+     _Set = Set;
+   } else {
+     // a non-standard Set polyfill that only works with primitive keys.
+     //设置一个set;
+
+
+
+    //  Set {constructor: ƒ, has: ƒ, add: ƒ, delete: ƒ, clear: ƒ, …}add: ƒ add()clear: ƒ clear()constructor: ƒ Set()delete: ƒ delete()entries: ƒ entries()arguments: (...)caller: (...)length: 0name: "entries"[[Prototype]]: ƒ ()[[Scopes]]: Scopes[0]forEach: ƒ forEach()has: ƒ has()keys: ƒ values()size: (...)values: ƒ values()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.toStringTag): "Set"get size: ƒ size()[[Prototype]]: Object
+     _Set = /*@__PURE__*/(function () {
+       function Set () {
+         this.set = Object.create(null);
+       }
+       Set.prototype.has = function has (key) {
+         return this.set[key] === true
+       };
+       Set.prototype.add = function add (key) {
+         this.set[key] = true;
+       };
+       Set.prototype.clear = function clear () {
+         this.set = Object.create(null);
+       };
+ 
+       return Set;
+     }());
+   }
+   
+
+
